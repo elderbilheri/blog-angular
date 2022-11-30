c09c15884cebe76f4371e120f8b5f016ae76d1ff
Elder Bilheri <elderbilheri@gmail.com>
2022-11-30 17:34:04 -0300

    styles update

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,23 +3,23 @@ export const dataFake = [
     id: '1',
     title: 'Brasil Classificado!',
     description:
-      'O Brasil se classificou na Copa do Mundo de 2022 que ocorre no Catar. A classificação foi confirmada nesta segunda-feira, após vencer a seleção da Suiça por 1x0 com gol de Casimiro.',
+      'O Brasil já garantiu vaga nas oitavas de final da Copa do Mundo do Qatar. Com a vitória suada em cima da Suíça, com golaço de Casemiro, o time de Tite já se garantiu entre as duas melhores equipes do grupo G. Com duas vitórias em dois jogos, o Brasil lidera a chave, sendo seguido pela Suíça, que possui três pontos. Camarões e Sérvia ocupam as últimas colocações, com apenas um ponto cada. Desta forma, o Brasil pode apenas ser ultrapasado pela Suíça, tendo garantido pelo menos o segundo lugar do grupo. Um resultado de empate ou vitória na última rodada, contra Camarões, dará a liderança da chave ao time canarinho.',
     photoCover:
       'https://images.unsplash.com/photo-1529629468183-b9cddd7be13b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     id: '2',
-    title: 'Copa do Mundo 2022: Veja os resultados de hoje!',
+    title: 'Tunísia derrota França, mas está eliminada da Copa do Mundo',
     description:
-      'Quatro partidas agitam o dia e determinam os classificados e eliminados dos Grupos A e B',
+      'Wahbi Khazri, nascido na França, marcou o único gol da partida na vitória da Tunísia sobre a França por 1 x 0 no estádio Cidade da Educação, nesta quarta-feira, mas a surpreendente vitória não foi suficiente para eles se juntarem à atual campeã mundial nas oitavas de final da Copa do Mundo no Catar. Khazri marcou o gol aos 13 minutos do segundo tempo em meio aos defensores, garantindo apenas a terceira vitória da Tunísia em seis Copas do Mundo. A França, que fez nove alterações para esta partida em relação ao time que venceu a Dinamarca e já havia garantido sua vaga nas oitavas de final, terminou na liderança do Grupo D no saldo de gols, à frente da Austrália, que venceu a Dinamarca por 1 x 0 na mesma rodada.A França agora enfrenta o segundo colocado do Grupo C, que será concluído ainda nesta quarta-feira.',
     photoCover:
-      'https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F1125%2Fr1096902_1296x729_16%2D9.jpg&w=920&h=518&scale=crop&cquality=80&location=origin&format=jpg',
+      'https://www.rbsdirect.com.br/imagesrc/35788075.jpg?w=700&rv=2-10-05&safari',
   },
   {
     id: '3',
     title: 'Modelo croata chama atenção no Catar',
     description:
-      'Ex-miss da Croácia, Ivana Knoll tem desfilado em estádios e pontos turísticos de Doha com roupas quadriculadas nas cores da bandeira do seu país',
+      'A principal candidata a musa da Copa do Mundo de 2022, no Catar, é a modelo croata Ivana Knoll, de 26 anos. Depois de comparecer a jogos dos Mundiais de 2014, no Brasil, e 2018, na Rússia, ela chama atenção em atrações turísticas de Doha e nos estádios desta edição por sua beleza e pelos trajes quadriculados nas cores da bandeira do seu país. Nas redes da modelo croata há comentários de admiradores, mas também muitas críticas de muçulmanos, que tratam suas fotos como desrespeito à cultura e aos hábitos do Catar. Em uma entrevista, Ivana Knoll se disse surpresa com as regras do Catar. "Eu ouvi sobre as regras e fiquei chocada. O código de vestimenta proíbe mostrar ombros, joelhos, barriga e tudo e eu fiquei tipo (Oh meu Deus, eu nem tenho roupas para cobrir tudo isso)", brincou.',
     photoCover:
       'https://i.superesportes.com.br/5rvrMjh0Owb6z51miD9mlK2zb3k=/650x0/smart/imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2022/11/29/3981757/ivana-knoll-modelo-croata_1_70843.jpg',
   },
@@ -28,7 +28,7 @@ export const dataFake = [
     title:
       'Senegal vence Equador e avança às oitavas de final da Copa do Mundo',
     description:
-      'Africanos vencem por 2 a 1, garantem segunda colocação do Grupo A e eliminam equatorianos do Mundial do Catar; Holanda também avança',
+      'Senegal está de volta ao mata-mata da Copa do Mundo após 20 anos. Nesta terça-feira, a Seleção Senegalesa venceu o Equador por 2 a 1, no Estádio Internacional Khalifa, pela terceira rodada da fase de grupos do Mundial do Catar. Ismalla Sarr e Koulibaly marcaram para os Leões da Terenga, enquanto Caicedo descontou. Com o placar, Senegal avançou às oitavas de final. Os africanos ficaram na segunda colocação do Grupo A, com seis pontos, dois a mais que os equatorianos, que ficaram na terceira posição e foram eliminados. A Holanda ficou na liderança da chave, com sete. O Catar, por sua vez, terminou na lanterna, com zero. Senegal volta a campo agora no domingo, às 16 horas (de Brasília), no Estádio Al Bayt, contra o primeiro colocado do Grupo B, que será definido nesta tarde.',
     photoCover:
       'https://i.superesportes.com.br/KNsELBI0W8MKEokqLnIalkdWzm8=/650x0/smart/imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2022/11/29/3981739/senegal-vai-as-oitavas-de-final_1_96785.jpg',
   },
